47bdd96b48843828e967f573c9beea2ab13df3ef
Andrea Ortez <andreaortez11@unitec.edu>
2023-10-30 22:51:21 -0600

    mas gui

--- src/proyecto_edd2/Main_Screen.java
+++ src/proyecto_edd2/Main_Screen.java
@@ -1,5 +1,8 @@
 package proyecto_edd2;
 
+import java.awt.Color;
+import javax.swing.JPanel;
+
 public class Main_Screen extends javax.swing.JFrame {
 
     public Main_Screen() {
@@ -8,6 +11,8 @@ public class Main_Screen extends javax.swing.JFrame {
         this.pack();
 
         Inicio.setVisible(true);
+        pb.setVisible(true);
+        Menu.setVisible(false);
         //Titulo.setVisible(true);
 
         AdmBarra h = new AdmBarra(pb);
@@ -17,6 +22,9 @@ public class Main_Screen extends javax.swing.JFrame {
         if (!h.isAlive()) {
             pb.setVisible(false);
             start.setVisible(true);
+        }else{
+            start.setVisible(false);
+            pb.setVisible(true);
         }
     }
 
@@ -24,6 +32,19 @@ public class Main_Screen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Menu = new javax.swing.JPanel();
+        MenuBar = new javax.swing.JPanel();
+        ArchivosButton = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        CamposButton = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        RegistrosButton = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        IndicesButton = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        EstandarizacionButton = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        Fondo1 = new javax.swing.JLabel();
         Inicio = new javax.swing.JPanel();
         start = new javax.swing.JLabel();
         pb = new javax.swing.JProgressBar();
@@ -32,16 +53,181 @@ public class Main_Screen extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        Menu.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        MenuBar.setBackground(new java.awt.Color(17, 30, 58));
+        MenuBar.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        ArchivosButton.setBackground(new java.awt.Color(17, 30, 58));
+        ArchivosButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                ArchivosButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                ArchivosButtonMouseExited(evt);
+            }
+        });
+
+        jLabel1.setBackground(new java.awt.Color(17, 30, 58));
+        jLabel1.setFont(new java.awt.Font("Roboto Medium", 0, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Archivos");
+
+        javax.swing.GroupLayout ArchivosButtonLayout = new javax.swing.GroupLayout(ArchivosButton);
+        ArchivosButton.setLayout(ArchivosButtonLayout);
+        ArchivosButtonLayout.setHorizontalGroup(
+            ArchivosButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+        );
+        ArchivosButtonLayout.setVerticalGroup(
+            ArchivosButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(ArchivosButtonLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+
+        MenuBar.add(ArchivosButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 160, 300, -1));
+
+        CamposButton.setBackground(new java.awt.Color(17, 30, 58));
+        CamposButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                CamposButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                CamposButtonMouseExited(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Roboto Medium", 0, 36)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Campos");
+
+        javax.swing.GroupLayout CamposButtonLayout = new javax.swing.GroupLayout(CamposButton);
+        CamposButton.setLayout(CamposButtonLayout);
+        CamposButtonLayout.setHorizontalGroup(
+            CamposButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+        );
+        CamposButtonLayout.setVerticalGroup(
+            CamposButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(CamposButtonLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel3)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+
+        MenuBar.add(CamposButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 250, 300, -1));
+
+        RegistrosButton.setBackground(new java.awt.Color(17, 30, 58));
+        RegistrosButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                RegistrosButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                RegistrosButtonMouseExited(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Roboto Medium", 0, 36)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Registros");
+
+        javax.swing.GroupLayout RegistrosButtonLayout = new javax.swing.GroupLayout(RegistrosButton);
+        RegistrosButton.setLayout(RegistrosButtonLayout);
+        RegistrosButtonLayout.setHorizontalGroup(
+            RegistrosButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+        );
+        RegistrosButtonLayout.setVerticalGroup(
+            RegistrosButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RegistrosButtonLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel4)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+
+        MenuBar.add(RegistrosButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 340, 300, -1));
+
+        IndicesButton.setBackground(new java.awt.Color(17, 30, 58));
+        IndicesButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                IndicesButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                IndicesButtonMouseExited(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Roboto Medium", 0, 36)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Índices");
+
+        javax.swing.GroupLayout IndicesButtonLayout = new javax.swing.GroupLayout(IndicesButton);
+        IndicesButton.setLayout(IndicesButtonLayout);
+        IndicesButtonLayout.setHorizontalGroup(
+            IndicesButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+        );
+        IndicesButtonLayout.setVerticalGroup(
+            IndicesButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(IndicesButtonLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel2)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+
+        MenuBar.add(IndicesButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 430, 300, -1));
+
+        EstandarizacionButton.setBackground(new java.awt.Color(17, 30, 58));
+        EstandarizacionButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                EstandarizacionButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                EstandarizacionButtonMouseExited(evt);
+            }
+        });
+
+        jLabel5.setFont(new java.awt.Font("Roboto Medium", 0, 36)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Estandarización");
+
+        javax.swing.GroupLayout EstandarizacionButtonLayout = new javax.swing.GroupLayout(EstandarizacionButton);
+        EstandarizacionButton.setLayout(EstandarizacionButtonLayout);
+        EstandarizacionButtonLayout.setHorizontalGroup(
+            EstandarizacionButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+        );
+        EstandarizacionButtonLayout.setVerticalGroup(
+            EstandarizacionButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(EstandarizacionButtonLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel5)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+
+        MenuBar.add(EstandarizacionButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 520, 300, -1));
+
+        Menu.add(MenuBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 300, 770));
+
+        Fondo1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Fondo.png"))); // NOI18N
+        Menu.add(Fondo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
         Inicio.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        start.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         start.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Start Button.png"))); // NOI18N
-        Inicio.add(start, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 360, 1350, 130));
+        start.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                startMouseClicked(evt);
+            }
+        });
+        Inicio.add(start, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 360, 300, 130));
 
         pb.setForeground(new java.awt.Color(255, 255, 255));
         Inicio.add(pb, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 370, 640, 40));
 
-        Titulo.setFont(new java.awt.Font("Dialog", 3, 70)); // NOI18N
+        Titulo.setFont(new java.awt.Font("Roboto Black", 3, 70)); // NOI18N
         Titulo.setForeground(new java.awt.Color(255, 255, 255));
         Titulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Titulo.setText("STANDARD FILE MANAGER");
@@ -55,14 +241,68 @@ public class Main_Screen extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(Inicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(Menu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(Inicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(Menu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void startMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_startMouseClicked
+        Menu.setVisible(true);
+    }//GEN-LAST:event_startMouseClicked
+
+    private void ArchivosButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ArchivosButtonMouseEntered
+        ArchivosButton.setBackground(new Color(255, 87, 87));
+    }//GEN-LAST:event_ArchivosButtonMouseEntered
+
+    private void ArchivosButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ArchivosButtonMouseExited
+        ArchivosButton.setBackground(new Color(17,30,58));
+    }//GEN-LAST:event_ArchivosButtonMouseExited
+
+    private void CamposButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CamposButtonMouseEntered
+        CamposButton.setBackground(new Color(251, 147, 67));
+    }//GEN-LAST:event_CamposButtonMouseEntered
+
+    private void CamposButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CamposButtonMouseExited
+        CamposButton.setBackground(new Color(17,30,58));
+    }//GEN-LAST:event_CamposButtonMouseExited
+
+    private void RegistrosButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RegistrosButtonMouseEntered
+        RegistrosButton.setBackground(new Color(27, 199, 78));
+    }//GEN-LAST:event_RegistrosButtonMouseEntered
+
+    private void RegistrosButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RegistrosButtonMouseExited
+        RegistrosButton.setBackground(new Color(17,30,58));
+    }//GEN-LAST:event_RegistrosButtonMouseExited
+
+    private void IndicesButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_IndicesButtonMouseEntered
+        IndicesButton.setBackground(new Color(0, 151, 178));
+    }//GEN-LAST:event_IndicesButtonMouseEntered
+
+    private void IndicesButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_IndicesButtonMouseExited
+        IndicesButton.setBackground(new Color(17,30,58));
+    }//GEN-LAST:event_IndicesButtonMouseExited
+
+    private void EstandarizacionButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_EstandarizacionButtonMouseEntered
+        EstandarizacionButton.setBackground(new Color(140, 82, 255));
+    }//GEN-LAST:event_EstandarizacionButtonMouseEntered
+
+    private void EstandarizacionButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_EstandarizacionButtonMouseExited
+        EstandarizacionButton.setBackground(new Color(17,30,58));
+    }//GEN-LAST:event_EstandarizacionButtonMouseExited
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -97,9 +337,22 @@ public class Main_Screen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel ArchivosButton;
+    private javax.swing.JPanel CamposButton;
+    private javax.swing.JPanel EstandarizacionButton;
     private javax.swing.JLabel Fondo;
+    private javax.swing.JLabel Fondo1;
+    private javax.swing.JPanel IndicesButton;
     private javax.swing.JPanel Inicio;
+    private javax.swing.JPanel Menu;
+    private javax.swing.JPanel MenuBar;
+    private javax.swing.JPanel RegistrosButton;
     private javax.swing.JLabel Titulo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JProgressBar pb;
     private javax.swing.JLabel start;
     // End of variables declaration//GEN-END:variables
